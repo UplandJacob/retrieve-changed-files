9230cab8b06bb6860383d48654e8db97c7521935
UplandJacob <123428589+UplandJacob@users.noreply.github.com>
2026-06-29 12:36:23 -0500

    Hopefully fix Jest

--- jest.config.js
+++ jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   testMatch: ['**/*.test.ts'],
   testRunner: 'jest-circus/runner',
   transform: {
-    '^.+\\.ts$': 'ts-jest'
+    '^.+\\.ts$': ['ts-jest', {tsconfig: 'tsconfig.jest.json'}]
   },
   verbose: true
 }
